1e3079f074a062008c0d01a1c0ff538c966ea2de
HitaPancaro <pancarohita0@gmail.com>
2024-09-24 21:09:47 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Scroll smooth ke bagian yang diinginkan
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
